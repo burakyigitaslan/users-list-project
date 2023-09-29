55172297f211f6fef8ca98218296879ca6233bc3
Burak Yigitaslan <10brk506@gmail.com>
2023-09-29 22:18:47 +0300

    completed

---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./UserList.module.css";
+import Card from "../UI/Card";
+
+const UsersList = (props) => {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => {
+          return (
+            <li key={user.id}>
+              {user.name} ({user.age} years old)
+            </li>
+          );
+        })}
+      </ul>
+    </Card>
+  );
+};
+
+export default UsersList;
